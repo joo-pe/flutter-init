50a3157f2e473d13a5326d83e563e79a2bdee7ec
박주형 <joohyung1002@naver.com>
2025-01-18 13:02:24 +0900

    fix: add

---
+++ lib/main.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(
+    MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: Scaffold(
+        backgroundColor: Color(0xFF335CB0),
+        body: Image.asset('asset/image/aa.png'),
+      ),
+    ),
+  );
+}
