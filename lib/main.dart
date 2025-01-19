09ac1c99d29da47fbf9c062a9f54e2d3b863a525
박주형 <joohyung1002@naver.com>
2025-01-19 10:43:36 +0900

    fix: appllied SatelessWidget 단축키 적용

--- lib/main.dart
+++ lib/main.dart
@@ -4,20 +4,29 @@ void main() {
   runApp(
     MaterialApp(
       debugShowCheckedModeBanner: false,
-      home: Scaffold(
-        backgroundColor: Color(0xFF335CB0),
-        body: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            Image.asset(
-              '',
-            ),
-            CircularProgressIndicator(
-              color: Colors.white,
-            )
-          ]
-        )
-      ),
+      home: HomeScreen(),
     ),
   );
+}
+
+class HomeScreen extends StatelessWidget {
+  const HomeScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        backgroundColor: Color(0xFF335CB0),
+        body: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Image.asset(
+                'asset/image/aa.jpeg',
+              ),
+              CircularProgressIndicator(
+                color: Colors.white,
+              )
+            ]
+        )
+    );
+  }
 }
