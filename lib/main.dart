f0bd302a66223a11ef8a8ff8d0e9f9ad8910996b
박주형 <joohyung1002@naver.com>
2025-01-21 08:47:13 +0900

    fix: mainaxisalignment test

--- lib/main.dart
+++ lib/main.dart
@@ -1,32 +1,11 @@
 import 'package:flutter/material.dart';
+import 'package:hello_world/screen/home_screen.dart';
 
 void main() {
   runApp(
-    MaterialApp(
-      debugShowCheckedModeBanner: false,
-      home: HomeScreen(),
-    ),
+      MaterialApp(
+          home: HomeScreen(),
+      ),
   );
 }
 
-class HomeScreen extends StatelessWidget {
-  const HomeScreen({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-        backgroundColor: Color(0xFF335CB0),
-        body: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              Image.asset(
-                'asset/image/aa.jpeg',
-              ),
-              CircularProgressIndicator(
-                color: Colors.white,
-              )
-            ]
-        )
-    );
-  }
-}
