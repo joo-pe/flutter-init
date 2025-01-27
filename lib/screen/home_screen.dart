ec68bd8d4441096de1abe91dd54a3d573fca0292
박주형 <joohyung1002@naver.com>
2025-01-27 11:04:27 +0900

    fix: StatefulWidget

--- lib/screen/home_screen.dart
+++ lib/screen/home_screen.dart
@@ -1,24 +1,49 @@
 import 'package:flutter/material.dart';
-import 'package:webview_flutter/webview_flutter.dart';
-import '../const/colors.dart';
 
-final homeUrl = Uri.parse('https://blog.codefactory.ai');
+class HomeScreen extends StatefulWidget {
 
-class HomeScreen extends StatelessWidget {
-  WebViewController controller = WebViewController()
-    ..loadRequest(homeUrl);
+  const HomeScreen({super.key});
 
-  HomeScreen({super.key});
+  @override
+  State<HomeScreen> createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+  Color color = Colors.blue;
 
   @override
   Widget build(BuildContext context) {
+    // TODO: implement build
     return Scaffold(
-        appBar: AppBar(
-          backgroundColor: Colors.orange,
-          title: Text('Jaden Factory'),
-          centerTitle: true,
-        ),
-        body: WebViewWidget(controller: controller),
-      );
+      body: Container(
+        width: double.infinity,
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            ElevatedButton(
+                onPressed: (){
+                  if(color == Colors.blue){
+                    color = Colors.red;
+                  }else if(color == Colors.red){
+                    color = Colors.blue;
+                  }
+                  setState(() {
+
+                  });
+                },
+                child: Text(
+                  '색상 변경!',
+                ),
+            ),
+            SizedBox(height: 32.0,),
+            Container(
+              width: 50.0,
+              height:  50.0,
+              color: color,
+            )
+          ]
+        )
+      )
+    );
   }
 }
---
+++ lib/screen/inweb_screen.dart
@@ -0,0 +1,35 @@
+import 'package:flutter/material.dart';
+import 'package:webview_flutter/webview_flutter.dart';
+import '../const/colors.dart';
+
+final homeUrl = Uri.parse('https://rnt-runnote-front.vercel.app');
+
+class HomeScreen extends StatelessWidget {
+  WebViewController controller = WebViewController()
+    ..setJavaScriptMode(JavaScriptMode.unrestricted)
+    ..loadRequest(homeUrl);
+
+  HomeScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Colors.orange,
+        title: Text('Jaden Factory'),
+        centerTitle: true,
+        actions: [
+          IconButton(
+            onPressed: (){
+              controller.loadRequest(homeUrl);
+            },
+            icon: Icon(
+              Icons.home,
+            ),
+          )
+        ],
+      ),
+      body: WebViewWidget(controller: controller),
+    );
+  }
+}
