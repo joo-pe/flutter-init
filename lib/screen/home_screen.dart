099f145de4c29e6980f3f967a98122a5fabb19b7
박주형 <joohyung1002@naver.com>
2025-01-29 16:26:49 +0900

    fix: image loading

--- lib/screen/home_screen.dart
+++ lib/screen/home_screen.dart
@@ -1,74 +1,21 @@
+import 'dart:async';
 import 'package:flutter/material.dart';
 
-class HomeScreen extends StatefulWidget {
-
-
+class HomeScreen extends StatelessWidget {
   const HomeScreen({super.key});
-
-  @override
-  State<HomeScreen> createState() => _HomeScreenState();
-}
-
-class _HomeScreenState extends State<HomeScreen> {
-  bool show = true;
 
   @override
   Widget build(BuildContext context) {
     // TODO: implement build
     return Scaffold(
-      body: SizedBox(
-        width: double.infinity,
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            if(show) CodeFactoryWidget(),
-            SizedBox(height: 32.0),
-            ElevatedButton(
-                onPressed: (){
-                  setState(() {
-                    show = !show;
-                  });
-                },
-                child: Text('클릭해서 보이기/안보이기')
-            )
-          ],
-        ),
-      )
+      body: PageView(
+        children: [1, 2, 3, 4, 5]
+          .map(
+              (e) => Image.asset(
+                  'asset/image/$e.jpeg', fit: BoxFit.cover,))
+          .toList(),
+    )
     );
   }
-}
 
-class CodeFactoryWidget extends StatefulWidget{
-  CodeFactoryWidget({super.key}){
-    print('1) stateful constructor');
-  }
-
-  @override
-  State<CodeFactoryWidget> createState() {
-    print('2) stateful creteState');
-
-    return  _CodeFactoryWidgetState();
-  }
-}
-
-class _CodeFactoryWidgetState extends State<CodeFactoryWidget> {
-
-  bool kDebugMode = true;
-
-  @override
-  void initState() {
-    if (kDebugMode) {
-      print('3) stateful widget initstate');
-    }
-    super.initState();
-  }
-  @override
-  Widget build(BuildContext context) {
-    // TODO: implement build
-    return Container(
-      width: 50.0,
-      height: 50.0,
-      color: Colors.red,
-    );
-  }
 }
